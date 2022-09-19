011c6513aa366d434eb7319704380a2d77de9761
Ashton Pooley <ashton.p404@gmail.com>
2022-09-19 22:18:29 +0930

    Merged taxa based input nt_batch with pylint ver.

--- nt_batch.py
+++ nt_batch.py
@@ -1,9 +1,9 @@
 """
-Aligns references and runs mafft align on all genes of each taxa within a folder of taxa folders.
+Placeholder module docstring
 
-PyLint 8.38/10
+Aligns references and runs mafft on all genes of a taxa -i
+PyLint 9.24/10
 """
-
 import argparse
 import os
 from multiprocessing.pool import ThreadPool
@@ -14,12 +14,12 @@ def run_command(arg_tuple: tuple) -> None:
     """
     Aligns the references and calls mafft on the target gene
     """
-    string, gene, taxa, parent, aa_path, nt_path, lock = arg_tuple
+    string, gene, aa_path, nt_path, lock = arg_tuple
 
     align_references(aa_path, nt_path)
     with lock:
         print(gene, aa_path, nt_path)
-    COMMAND = string.format(gene, taxa, parent)
+    COMMAND = string.format(gene)
     os.system(COMMAND)
 
 
@@ -35,7 +35,6 @@ def align_references(aa_path: str, nt_path: str) -> None:
                     fields = line.split("|")
                     if len(fields) <= 3:
                         order.append(fields[1]) # Save order of reference taxa name
-
                     else:
                         break
 
@@ -71,55 +70,40 @@ def align_references(aa_path: str, nt_path: str) -> None:
     with open(nt_path, "w", encoding = "UTF-8") as nt_out:
         nt_out.write("\n".join(nt_out_lines))
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-i", "--input", type=str, default="Parent", help="Parent input path."
-    )
-    parser.add_argument(
-        "-p",
-        "--processes",
-        type=int,
-        default=0,
-        help="Number of threads used to call processes.",
-    )
-    args = parser.parse_args()
 
-    COMMAND = "perl pal2nal.mod.pl -output fasta {2}/{1}/mafft/{0}.aa.fa {2}/{1}/nt/{0}.nt.fa > {2}/{1}/nt_aligned/{0}.nt.fa"
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-p",
+    "--processes",
+    type=int,
+    default=0,
+    help="Number of threads used to call processes.",
+)
+args = parser.parse_args()
 
-    for taxa in os.listdir(args.input):
-        print(f"Doing taxa {taxa}")
-        taxa_path = os.path.join(args.input, taxa)
-        nt_aligned_path = os.path.join(taxa_path, "nt_aligned")
-        mafft_path = os.path.join(taxa_path, "mafft")
+if not os.path.exists("nt_aligned"):
+    os.mkdir("nt_aligned")
 
-        if os.path.exists(mafft_path):
-            if not os.path.exists(nt_aligned_path):
-                os.mkdir(nt_aligned_path)
+genes = [gene.split(".")[0] for gene in os.listdir("mafft") if ".aa" in gene]
 
-            genes = [gene.split(".")[0] for gene in os.listdir(mafft_path) if ".aa" in gene]
+COMMAND = "perl pal2nal.mod.pl -output fasta mafft/{0}.aa.fa nt/{0}.nt.fa > nt_aligned/{0}.nt.fa"
 
-            if args.processes:
-                arguments = []
-                lock = Lock()
-                for gene in genes:
+if args.processes:
+    arguments = []
+    lock = Lock()
+    for target_gene in genes:
+        target_aa_path = os.path.join("mafft", target_gene + ".aa.fa")
+        target_nt_path = os.path.join("nt", target_gene + ".nt.fa")
 
-                    aa_path = os.path.join(args.input, taxa, "mafft", gene + ".aa.fa")
-                    nt_path = os.path.join(args.input, taxa, "nt", gene + ".nt.fa")
+        arguments.append((COMMAND, target_gene, target_aa_path, target_nt_path, lock))
+        arguments.append((COMMAND, target_gene, lock))
+    with ThreadPool(args.processes) as pool:
+        pool.map(run_command, arguments, chunksize=1)
+else:
+    for target_gene in genes:
+        print(target_gene)
+        target_aa_path = os.path.join("mafft", target_gene + ".aa.fa")
+        target_nt_path = os.path.join("nt", target_gene + ".nt.fa")
 
-                    arguments.append(
-                        (COMMAND, gene, taxa, args.input, aa_path, nt_path, lock)
-                    )
-                with ThreadPool(args.processes) as pool:
-                    pool.map(run_command, arguments, chunksize=1)
-            else:
-                for gene in genes:
-                    print(gene)
-
-                    aa_path = os.path.join(args.input, taxa, "mafft", gene + ".aa.fa")
-                    nt_path = os.path.join(args.input, taxa, "nt", gene + ".nt.fa")
-
-                    align_references(aa_path, nt_path)
-                    os.system(COMMAND.format(gene, taxa, args.input))
-        else:
-            print("Can't find mafft folder for taxa {}".format(taxa))
+        align_references(target_aa_path, target_nt_path)
+        os.system(COMMAND.format(target_gene))
