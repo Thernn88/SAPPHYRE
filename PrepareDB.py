7186d2af0b7de5ab8ec61eea14b217268b347a4f
Thernn88 <39282665+Thernn88@users.noreply.github.com>
2022-09-22 10:03:10 -0400

    Update PrepareDB.py

--- PrepareDB.py
+++ PrepareDB.py
@@ -9,6 +9,10 @@ import hashlib
 from tqdm import tqdm
 import math
 
+def rev_comp(seq):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    return "".join(complement.get(base, base) for base in reversed(seq))
+
 def main(argv):
     global_start = time()
     parser = argparse.ArgumentParser()
@@ -25,7 +29,7 @@ def main(argv):
         "-k",
         "--keep_prepared",
         action="store_true",
-        help="Keep prepared input file in new directory.",
+        help="Output prepared & merged fasta in taxa directory.",
     )
     parser.add_argument(
         "-m",
@@ -44,10 +48,13 @@ def main(argv):
 
     MAX_PREPARED_LEVEL_SIZE = args.max_prepared_batch_size
 
+    allowed_filetypes = ['fa', 'fas', 'fasta']
+
     rocksdb_folder_name = "rocksdb"
     core_directory = "PhyMMR"
     secondary_directory = os.path.join(core_directory, os.path.basename(args.input))
 
+    # Create necessary directories
     if args.verbose != 0:
         print("Creating directories")
 
@@ -57,134 +64,176 @@ def main(argv):
     if not os.path.exists(secondary_directory):
         os.mkdir(secondary_directory)
 
+    taxa_runs = {}
+    rev_comp_save = {}
+
+    # Scan all the files in the input. Remove _R# and merge taxa
     for file in os.listdir(args.input):
-        if ".fa" in file:
-            taxa_start = time()
-            if args.verbose != 0:
-                print(f"Preparing {file}")
+        if os.path.isfile(os.path.join(args.input, file)) and file.split('.')[-1] in allowed_filetypes:
+            taxa = file.split('.')[0]
+            if taxa[-1].isnumeric() and taxa[-2] == 'R' and taxa[-3] == '_': # Contains "_R#"
+                formatted_taxa = taxa[:-3]+'.fa'
+            else:
+                formatted_taxa = file
 
-            fa_file_directory = os.path.join(args.input, file)
-            taxa_destination_directory = os.path.join(secondary_directory, file)
+            if formatted_taxa in taxa_runs:
+                taxa_runs[formatted_taxa].append(file)
+            else:
+                taxa_runs[formatted_taxa] = [file]
+    
+    for formatted_taxa_out, components in taxa_runs.items():
+        taxa_start = time()
+        if args.verbose != 0:
+            print(f"Preparing {formatted_taxa_out}")
 
-            if not os.path.exists(taxa_destination_directory):
+        taxa_destination_directory = os.path.join(secondary_directory, formatted_taxa_out)
+        rocksdb_path = os.path.join(taxa_destination_directory, rocksdb_folder_name)
+
+        if args.clear_database:
+            if os.path.exists(rocksdb_path):
+                if args.verbose != 0:
+                    print("Clearing old database")
+                rmtree(rocksdb_path)
+
+        if args.verbose >= 1:
+            print("Creating rocksdb database")
+
+        db = wrap_rocks.RocksDB(rocksdb_path)
+
+        if not os.path.exists(taxa_destination_directory):
                 os.mkdir(taxa_destination_directory)
 
-            prepared_file_destination = os.path.join(taxa_destination_directory, file)
+        prepared_file_destination = os.path.join(taxa_destination_directory, formatted_taxa_out)
+        
+        duplicates = {}
+        dupe_set = set()
+        dupes = 0
+        prepared_component_all = []
+        sequence_count = 0
 
-            if not os.path.exists(prepared_file_destination):
-                with open(
-                    prepared_file_destination, "w", encoding="UTF-8"
-                ) as fa_file_out:
-                    with open(fa_file_directory, encoding="UTF-8") as fa_file_in:
-                        for line in fa_file_in:
-                            line = line.strip()
-                            if line != "":
-                                fa_file_out.write(line + "\n")
+        if args.verbose >= 1:
+            print("Formatting input sequences and inserting into database")
+        if args.keep_prepared:
+            fa_file_out = open(prepared_file_destination, "w", encoding="UTF-8")
+        for file in components:
+            fa_file_directory = os.path.join(args.input, file)
+            if ".fa" in file:
+                with open(fa_file_directory, encoding="UTF-8") as fa_file_in:
+                    lines = fa_file_in.readlines()
 
-            rocksdb_path = os.path.join(taxa_destination_directory, rocksdb_folder_name)
+                    if lines[-1] == "\n": lines = lines[:-1]
 
-            if args.clear_database:
-                if os.path.exists(rocksdb_path):
-                    if args.verbose != 0:
-                        print("Clearing old database")
-                    rmtree(rocksdb_path)
+                    sequence_count += int(len(lines) / 2)
 
-            if args.verbose >= 1:
-                print("Creating rocksdb database")
+                    for_loop_range = (
+                        tqdm(range(0, len(lines), 2))
+                        if args.verbose != 0
+                        else range(0, len(lines), 2)
+                    )
+                    
+                    for i in for_loop_range:
+                        header = lines[i].strip()
+                        seq = lines[i+1].strip()
 
-            db = wrap_rocks.RocksDB(rocksdb_path)
+                        # Check for dupe, if so save how many times that sequence occured
+                        seq_hash = hash(seq)
+                        if seq_hash in dupe_set:
+                            if seq_hash in duplicates:
+                                duplicates[seq_hash] += 1
+                            else:
+                                duplicates[seq_hash] = 2
+                            dupes += 1
+                            continue
+                        else:
+                            dupe_set.add(seq_hash)
 
-            prepared_component_all = []
-            duplicates = {}
-            dupe_set = set()
+                        # Rev-comp sequence. Save the reverse compliment in a hashmap with the original
+                        # sequence so we don't have to rev-comp this unique sequence again
+                        if seq in rev_comp_save:
+                            rev_seq = rev_comp_save[seq]
+                        else:
+                            rev_seq = rev_comp(seq)
+                            rev_comp_save[seq] = rev_seq
 
-            if args.verbose != 0:
-                print("Inserting sequences into database")
+                        # Check for revcomp dupe, if so save how many times that sequence occured
+                        seq_hash = hash(rev_seq)
+                        if seq_hash in dupe_set:
+                            if seq_hash in duplicates:
+                                duplicates[seq_hash] += 1
+                            else:
+                                duplicates[seq_hash] = 2
+                            dupes += 1
+                            continue
+                        else:
+                            dupe_set.add(seq_hash)
+                        
+                        # If no dupe, write to prepared file and db
+                        line = header+'\n'+seq+'\n'
 
-            with open(prepared_file_destination) as file_in:
-                lines = file_in.read().split("\n")
+                        if args.keep_prepared: # If IO output
+                            fa_file_out.write(line)
 
-            if lines[-1] == "":
-                lines = lines[:-1]
+                        # Save prepared file lines in a list to ration into the db
+                        prepared_component_all.append(line)
 
-            sequence_count = int(len(lines)/2)
+                        # Get rid of space and > in header (blast/hmmer doesn't like it)
+                        preheader = header.replace(" ", "|").replace(">", "") # pre-hash header
+                        
+                        # Data that will be stored in the database
+                        data = f"{preheader}\n{seq}"
 
-            for_loop_range = (
-                tqdm(range(0, len(lines), 2))
-                if args.verbose != 0
-                else range(0, len(lines), 2)
+                        # Hash the header
+                        header = hashlib.sha256(preheader.encode()).hexdigest()
+
+                        # Write to rocksdb
+                        db.put(header, data)
+
+        if args.keep_prepared:
+            fa_file_out.close()
+
+        if args.verbose != 0:
+            print(
+                "Inserted {} sequences for {} found {} dupes.\n".format(sequence_count-dupes, formatted_taxa, dupes)
             )
 
-            for i in for_loop_range:
-                header = lines[i]
-                sequence = lines[i + 1]
+        del dupe_set # Clear mem
 
-                prepared_component_all.append(header+'\n'+sequence+'\n')
+        if args.verbose != 0:
+            print("Storing prepared file in database")
+        
+        # Figure out how many levels are needed to split sequence_count into MAX_PREPARED_LEVEL_SIZE sized chunks 
+        prepared_file_levels = math.ceil(sequence_count / MAX_PREPARED_LEVEL_SIZE)
+        sequences_per_level = math.ceil(sequence_count / prepared_file_levels)
+        prepared_recipe = []
 
-                this_hash = hash(sequence)
-                if this_hash not in dupe_set:
-                    dupe_set.add(this_hash)
-                else:
-                    if sequence in duplicates:
-                        duplicates[sequence] += 1
-                    else:
-                        duplicates[sequence] = 2
+        # Grab the chunks of the prepared file lines and store in db
+        for i in range(0, prepared_file_levels):
+            start_index = i * sequences_per_level
+            end_index = (i + 1) * sequences_per_level
 
-                # get rid of space and > in header (blast/hmmer doesn't like it)
-                preheader = header.replace(" ", "|").replace(">", "") # pre-hash header
-                
-                data = f"{preheader}\n{sequence}"
+            this_batch = "".join(prepared_component_all[start_index:end_index])
+            key = f"prepared:{i}"
 
-                header = hashlib.sha256(preheader.encode()).hexdigest()
-                db.put(header, data)
+            db.put(key, this_batch)
 
-            if args.verbose != 0:
+            prepared_recipe.append(key)
 
-                print(
-                    "Inserted {} sequences for {}.\n".format(sequence_count, file)
-                )
+        # Store the keys to each component of the prepared file
+        key = "getall:prepared"
+        data = ",".join(prepared_recipe)
 
-            del dupe_set # Clear mem
+        db.put(key, data)
 
-            if args.verbose != 0:
-                print("Storing prepared file in database")
+        # Store the count of dupes in the database
+        key = "getall:dupes"
+        data = json.dumps(duplicates)
 
-            # store the keys to each component of the prepared file
-            
-            prepared_file_levels = math.ceil(sequence_count / MAX_PREPARED_LEVEL_SIZE)
-            sequences_per_level = math.ceil(sequence_count / prepared_file_levels)
-            prepared_recipe = []
+        db.put(key, data)
 
-            for i in range(0, prepared_file_levels):
-                start_index = i * sequences_per_level
-                end_index = (i + 1) * sequences_per_level
-
-                this_batch = "".join(prepared_component_all[start_index:end_index])
-                key = f"prepared:{i}"
-
-                db.put(key, this_batch)
-
-                prepared_recipe.append(key)
-
-            key = "getall:prepared"
-            data = ",".join(prepared_recipe)
-
-            db.put(key, data)
-
-            # store the count of dupes in the database
-            key = "getall:dupes"
-            data = json.dumps(duplicates)
-
-            db.put(key, data)
-
-            if args.verbose != 0:
-                print(
-                    "Cleaning up. Took {:.2f}s for {}".format(time() - taxa_start, file)
-                )
-
-            if args.keep_prepared is not True:
-                # We can delete these as it's stored in memory for future use if needed
-                os.remove(prepared_file_destination)
+        if args.verbose != 0:
+            print(
+                "Took {:.2f}s for {}".format(time() - taxa_start, file)
+            )
 
     print("Finished took {:.2f}s overall.".format(time() - global_start))
 
