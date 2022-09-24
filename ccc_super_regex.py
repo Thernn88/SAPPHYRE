c60f1cc7b04eceea5660a336296fac86c971ecc7
Ashton Pooley <ashton.p404@gmail.com>
2022-09-24 16:57:04 +0930

    PyLint formatted CCC_super_regex

--- ccc_super_regex.py
+++ ccc_super_regex.py
@@ -3,7 +3,6 @@ import os
 import re
 from sys import argv
 
-
 def truncate_taxa(header: str, extension=None) -> str:
     """
     Given a fasta header, checks the end for problematic tails.
@@ -14,7 +13,7 @@ def truncate_taxa(header: str, extension=None) -> str:
     result = header
     m = re.search(r"_R?\d+$", header)
     if m is not None:
-        tail_length = m.end()-m.start()
+        tail_length = m.end() - m.start()
         result = result[0:-tail_length]
     if extension is not None:
         result = result + extension
@@ -25,17 +24,28 @@ def folder_check(folder: str) -> None:
     if not os.path.exists(folder):
         os.mkdir(folder)
 
+
 def get_filenames(folder: str) -> list:
-    result = [item for item in os.listdir(folder) if item[0] not in ['.','$']]
+    """
+    Returns every file that does not start with . or $ in a given folder
+    """
+    result = [item for item in os.listdir(folder) if item[0] not in [".", "$"] and os.path.isfile(item)]
     return result
+
 
 def sequence_is_reference(header):
     """
     Returns True if last character of header is .
     """
-    return header[-1] == '.'
+    return header[-1] == "."
 
-def clone_and_clean_files(aa_fastas: list, out_dir_clean_aa: str, nt_fastas: str, out_dir_clean_nt: str) -> None:
+
+def clone_and_clean_files(
+    aa_fastas: list, out_dir_clean_aa: str, nt_fastas: str, out_dir_clean_nt: str
+) -> None:
+    """
+    Clones aa and nt files into clean directory with formatted headers
+    """
     rev_f_tags = {}
     anti_dupe_tags = {}
     anti_dupe_counts = {}
@@ -44,145 +54,172 @@ def clone_and_clean_files(aa_fastas: list, out_dir_clean_aa: str, nt_fastas: str
 
     for fasta in aa_fastas:
         this_gene = os.path.basename(fasta)
-        if this_gene not in aa_content:
-            aa_content[this_gene] = []
 
-        with open(fasta) as f:
-            for line in f:
-                if line != '\n':
-                    aa_content[this_gene].append(line.strip())
+        with open(fasta, encoding="UTF-8") as fasta_io:
+            lines = [line for line in fasta_io if line != "\n"]
 
-    for fasta in aa_content:
-        aa_new_file_path = os.path.join(out_dir_clean_aa,fasta)
-        gene = fasta.split('.')[0]
+            if this_gene in aa_content:
+                aa_content[this_gene].extend(lines)
+            else:
+                aa_content[this_gene] = lines
+
+    for fasta, lines in aa_content.items():
+
+        aa_new_file_path = os.path.join(out_dir_clean_aa, fasta)
+        gene = fasta.split(".")[0]
         if gene not in rev_f_tags:
             rev_f_tags[gene] = {}
             anti_dupe_tags[gene] = {}
 
-        lines = aa_content[fasta]
-        
-        headers = [line.split('|')[2] for line in lines if line[0] == '>']
-        output = open(aa_new_file_path, 'w')  # open this once instead of every loop iteration
-        already_written = set()
-        for i in range(0, len(lines), 2):
-            header = lines[i]
-            sequence = lines[i+1]
-            
-            header_gene, taxaId, node, coords, frame, taxa = header.split('|')
-            
-            if sequence_is_reference(header):
-                new_header = ''.join([header_gene, '|', taxaId, '|', node]) # its faster to change a list
-                if new_header in already_written:
-                    continue
-                already_written.add(new_header)
-            else:
-                if not taxaId[-1].isnumeric():
-                    taxaId = taxaId[:-1]
+        headers = [line.split("|")[2] for line in lines if line[0] == ">"]
+        end_of_references = False
+        with open(aa_new_file_path, "w", encoding="UTF-8") as output:
+            already_written = set()
+            for i in range(0, len(lines), 2):
+                header = lines[i].strip()
+                sequence = lines[i + 1].strip()
 
-                new_header = [header_gene, '|', taxa, '|', taxaId, '|', node]
-
-                if headers.count(node) > 1:
-                    if node not in rev_f_tags[gene]: rev_f_tags[gene][node] = {}
-
-                    if 'revcomp' in frame:
-                        rev_f_tags[gene][node][i] = '_R'
-                        new_header.append('_R')
+                if end_of_references is False:
+                    if sequence_is_reference(header):
+                        header_gene, taxa_name, taxa_id, identifier = header.split("|")
+                        new_header = "|".join(
+                            [header_gene, taxa_name, taxa_id, identifier]
+                        )  # its faster to change a list
+                        if new_header in already_written:
+                            continue
+                        already_written.add(new_header)
                     else:
-                        rev_f_tags[gene][node][i] = '_F'
-                        
-                        new_header.append('_F')
+                        end_of_references = True
 
-                    if not node in anti_dupe_tags[gene]: anti_dupe_tags[gene][node] = {}
+                if end_of_references is True:
+                    header_gene, taxa, taxa_id, node, _, frame = header.split("|")
+                    if not taxa_id[-1].isnumeric() and taxa_id[-2] == "_":
+                        taxa_id = taxa_id[:-2]
 
-                    if node in anti_dupe_counts:
-                        anti_dupe_counts[node] += 1
-                        count = anti_dupe_counts[node]
-                    else:
-                        anti_dupe_counts[node] = 1
-                        count = 1
+                    new_header = [header_gene, "|", taxa, "|", taxa_id, "|", node]
 
-                    header_addition = '_{}'.format(count)
-                    anti_dupe_tags[gene][node][i] = header_addition
+                    if headers.count(node) > 1:
+                        if node not in rev_f_tags[gene]:
+                            rev_f_tags[gene][node] = {}
 
-                    new_header.append(header_addition)
-                new_header = ''.join(new_header)
+                        if "revcomp" in frame:
+                            rev_f_tags[gene][node][i] = "_R"
+                            new_header.append("_R")
+                        else:
+                            rev_f_tags[gene][node][i] = "_F"
 
-            output.write(new_header)
+                            new_header.append("_F")
 
-            output.write('\n')
-            output.write(sequence)
-            output.write('\n')
+                        if not node in anti_dupe_tags[gene]:
+                            anti_dupe_tags[gene][node] = {}
 
-        output.close()
+                        if node in anti_dupe_counts:
+                            anti_dupe_counts[node] += 1
+                            count = anti_dupe_counts[node]
+                        else:
+                            anti_dupe_counts[node] = 1
+                            count = 1
+
+                        header_addition = f"_{count}"
+                        anti_dupe_tags[gene][node][i] = header_addition
+
+                        new_header.append(header_addition)
+
+                    new_header = "".join(new_header)
+
+                    output.write(new_header)
+                    output.write("\n")
+                    output.write(sequence)
+                    output.write("\n")
 
     nt_content = {}
 
     for fasta in nt_fastas:
         this_gene = os.path.basename(fasta)
-        if this_gene not in nt_content:
-            nt_content[this_gene] = []
 
-        with open(fasta) as f:
-            for line in f:
-                if line != '\n':
-                    nt_content[this_gene].append(line.strip())
+        with open(fasta, encoding="UTF-8") as fasta_io:
+            lines = [line for line in fasta_io if line != "\n"]
 
-    for fasta in nt_content:
-        gene = fasta.split('.')[0]
-
-        nt_new_file_path = os.path.join(out_dir_clean_nt,fasta)
-
-        lines = nt_content[fasta]
-
-        headers = [line.split('|')[2] for line in lines if '>' in line]
-        output = open(nt_new_file_path,'w')
-        already_written = set()
-        for i in range(0,len(lines),2):
-            header = lines[i]
-            sequence = lines[i+1]
-            
-            header_gene,taxaId,node,coords,frame,taxa = header.split('|')
-
-            if sequence_is_reference(header):
-                new_header = ''.join([header_gene, '|', taxaId, '|', node]) # Format reference header
-                if new_header in already_written:
-                    continue
-                already_written.add(new_header)
+            if this_gene in nt_content:
+                nt_content[this_gene].extend(lines)
             else:
-                if not taxaId[-1].isnumeric():
-                    taxaId = taxaId[:-1]
-                    
-                new_header = [header_gene, '|', taxa, '|', taxaId, '|', node] # Format candidate header
+                nt_content[this_gene] = lines
 
-                if headers.count(node) > 1:
-                    new_header.append(rev_f_tags[gene][node][i])
-                    new_header.append(anti_dupe_tags[gene][node][i])
-                new_header = ''.join(new_header)
-            output.write(new_header)
-            output.write('\n')
-            output.write(sequence)
-            output.write('\n')
+    for fasta, lines in nt_content.items():
+        gene = fasta.split(".")[0]
 
-        output.close()
+        nt_new_file_path = os.path.join(out_dir_clean_nt, fasta)
+
+        headers = [line.split("|")[2] for line in lines if ">" in line]
+
+        end_of_references = False
+        with open(nt_new_file_path, "w", encoding="UTF-8") as output:
+            already_written = set()
+            for i in range(0, len(lines), 2):
+                header = lines[i].strip()
+                sequence = lines[i + 1].strip()
+
+                if end_of_references is False:
+                    if sequence_is_reference(header):
+                        header_gene, taxa_name, taxa_id, identifier = header.split("|")
+                        new_header = "|".join(
+                            [header_gene, taxa_name, taxa_id, identifier]
+                        )  # Format reference header
+                        if new_header in already_written:
+                            continue
+                        already_written.add(new_header)
+                    else:
+                        end_of_references = True
+
+                if end_of_references is True:
+                    header_gene, taxa, taxa_id, node, _, frame = header.split("|")
+                    if not taxa_id[-1].isnumeric() and taxa_id[-2] == "_":
+                        taxa_id = taxa_id[:-2]
+
+                    new_header = [
+                        header_gene,
+                        "|",
+                        taxa,
+                        "|",
+                        taxa_id,
+                        "|",
+                        node,
+                    ]  # Format candidate header
+
+                    if headers.count(node) > 1:
+                        new_header.append(rev_f_tags[gene][node][i])
+                        new_header.append(anti_dupe_tags[gene][node][i])
+                    new_header = "".join(new_header)
+
+                output.write(new_header)
+                output.write("\n")
+                output.write(sequence)
+                output.write("\n")
+
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--input', required=True, help="Super folder")
-    parser.add_argument('-o','--output', required=True,
-                        help="Location of cloned directory")
+    parser.add_argument("-i", "--input", required=True, help="Super folder")
+    parser.add_argument(
+        "-o", "--output", required=True, help="Location of cloned directory"
+    )
     args = parser.parse_args()
 
     folder_check(args.output)
 
-    taxas = [taxa for taxa in os.listdir(args.input) if os.path.isdir(os.path.join(args.input, taxa))]
+    taxas = [
+        taxa
+        for taxa in os.listdir(args.input)
+        if os.path.isdir(os.path.join(args.input, taxa))
+    ]
     taxa_groups = dict()
 
     for taxa in taxas:
-        taxa_name = taxa.split('.')[0]
+        taxa_name = taxa.split(".")[0]
         taxa_name = truncate_taxa(taxa_name)
         if taxa_name not in taxa_groups:
             taxa_groups[taxa_name] = []
-        
+
         taxa_groups[taxa_name].append(taxa)
 
     for taxa in taxa_groups:
@@ -192,28 +229,27 @@ def main(argv):
         nt_fasta = []
 
         for combine_taxa in to_combine_taxa:
-            aa_in = os.path.join(os.path.join(args.input, combine_taxa), 'aa')
+            aa_in = os.path.join(os.path.join(args.input, combine_taxa), "aa")
             aa_fastas.extend([os.path.join(aa_in, aa) for aa in get_filenames(aa_in)])
 
-            nt_in = os.path.join(os.path.join(args.input, combine_taxa), 'nt')
+            nt_in = os.path.join(os.path.join(args.input, combine_taxa), "nt")
             nt_fasta.extend([os.path.join(nt_in, nt) for nt in get_filenames(nt_in)])
-        
+
         clone = os.path.join(args.output, taxa)
         folder_check(clone)
 
-        clean_directory = os.path.join(clone, 'clean')
+        clean_directory = os.path.join(clone, "clean")
 
         folder_check(clean_directory)
-        
-        aa_out = os.path.join(clone, 'aa')
-        nt_out = os.path.join(clone, 'nt')
-        aa_clean_out = os.path.join(clean_directory, 'aa')
-        nt_clean_out = os.path.join(clean_directory, 'nt')
+
+        aa_clean_out = os.path.join(clean_directory, "aa")
+        nt_clean_out = os.path.join(clean_directory, "nt")
 
         folder_check(aa_clean_out)
         folder_check(nt_clean_out)
 
         clone_and_clean_files(aa_fastas, aa_clean_out, nt_fasta, nt_clean_out)
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main(argv)
