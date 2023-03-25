6c8dc938be06ac429c88144afe1ab92bfe9bfed1
Ashton Pooley <ashton.p404@gmail.com>
2023-03-26 10:28:53 +1030

    Fixed variables out side of args

--- sapphyre/align.py
+++ sapphyre/align.py
@@ -405,7 +405,7 @@ def do_folder(folder, args):
     only_singletons = set()
     for gene in genes:
         under_safeguard = False
-        for _, seq in parseFasta(os.path.join(aln_path, args.gene + ".aln.fa")):
+        for _, seq in parseFasta(os.path.join(aln_path, gene + ".aln.fa")):
             if len(seq) >= SAFEGUARD_BP:
                 only_singletons.add(gene)
                 break
