0b3cf9c86246af56fa3a93a41d7cebf912a51f50
Michael Valentine <mvalentine65@gmail.com>
2023-08-20 15:44:00 -0400

    adds compress arg to final writeFasta call in align.py

--- sapphyre/align.py
+++ sapphyre/align.py
@@ -598,7 +598,7 @@ def run_command(args: CmdArgs) -> None:
 
     to_write.sort(key=lambda x: get_start(x[1]))
 
-    writeFasta(args.result_file, references + to_write)
+    writeFasta(args.result_file, references + to_write, compress=args.compress)
 
     printv(f"Done. Took {keeper.differential():.2f}", args.verbose, 3)  # Debug
 
