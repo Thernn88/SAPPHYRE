f2ee20bef5abed13e2f23aa975d8205f89b864eb
Ashton Pooley <ashton.p404@gmail.com>
2023-08-29 10:11:46 +0930

    Do ref skip to nt in rename

--- sapphyre/finalize.py
+++ sapphyre/finalize.py
@@ -142,7 +142,13 @@ def rename_taxon(aa_content: list, nt_content: list, taxa_to_taxon: dict) -> tup
             new_aa_content.append((header, seq))
         else:
             aa_candidates.append((header, seq))
-    for (aa_header, aa_line), (nt_header, nt_line) in zip(aa_candidates, nt_content):
+    nt_candidates = []
+    for header, seq in nt_content:
+        if header.endswith("."):
+            new_nt_content.append((header, seq))
+        else:
+            nt_candidates.append((header, seq))
+    for (aa_header, aa_line), (nt_header, nt_line) in zip(aa_candidates, nt_candidates):
     # for (aa_header, aa_line), (nt_header, nt_line) in zip(aa_content, nt_content):
         if not aa_header.endswith("."):
             if aa_header != nt_header:
