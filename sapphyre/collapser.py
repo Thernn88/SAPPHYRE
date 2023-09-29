c3425abb7044341628c3992914583315cd6f31c0
Kevin Moran <39282665+Thernn88@users.noreply.github.com>
2023-09-29 17:24:37 -0400

    Update collapser.py

--- sapphyre/collapser.py
+++ sapphyre/collapser.py
@@ -314,7 +314,7 @@ def process_batch(
 
         ref_average_data_length = sum(ref_average_data_length) / len(ref_average_data_length)
 
-        match_percent = args.matching_consensus_percent if batch_args.is_assembly else 0.6
+        match_percent = args.matching_consensus_percent
 
         for read in nodes:
             average_matching_cols = average_match(
