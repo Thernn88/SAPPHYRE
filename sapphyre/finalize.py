588f622aac95921a736f27cfb0e737762cca601b
Ashton Pooley <ashton.p404@gmail.com>
2023-11-12 10:41:22 +1030

    Fix most common AA logic

--- sapphyre/finalize.py
+++ sapphyre/finalize.py
@@ -261,8 +261,11 @@ def clean_gene(gene_config: GeneConfig):
         for col, chars in col_dict.items():
             this_most_common = Counter(chars).most_common()
             most_common_inclusive = this_most_common[0][1]
-            if this_most_common[0][0] == "-" and len(this_most_common) == 1:
-                most_common_AA_count = 0
+            if len(this_most_common) == 1:
+                if this_most_common[0][0] == "-": 
+                    most_common_AA_count = 0
+                else:
+                    most_common_AA_count = this_most_common[0][1]
             else:
                 most_common_AA_count =  this_most_common[0][1] if this_most_common[0][0] == "-" else this_most_common[1][1]
             column_stats[col] = len(chars), len(chars)-chars.count("-"), most_common_inclusive, most_common_AA_count            
