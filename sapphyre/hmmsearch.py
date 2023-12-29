42a2824f3506359c218773adbe8855a0a194050e
Ashton Pooley <ashton.p404@gmail.com>
2023-12-30 09:31:06 +1030

    Fixed -d crash

--- sapphyre/hmmsearch.py
+++ sapphyre/hmmsearch.py
@@ -111,7 +111,7 @@ def hmm_search(gene, diamond_hits, hmm_output_folder, hmm_location, overwrite, d
                 )
 
     if debug:
-        return "", []
+        return "", [], []
     data = {}
     with open(this_hmm_output) as f:
         for line in f:
