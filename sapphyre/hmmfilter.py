ac2311ffec678449863b8ebf487786259fde6c6f
Ashton Pooley <ashton.p404@gmail.com>
2024-02-06 17:57:22 +1030

    Revert "Kick overlap >= 80% and pool scores"
    This reverts commit 5e25dc7153aff34ea5497d8fd525d6000f07a100.

--- sapphyre/hmmfilter.py
+++ sapphyre/hmmfilter.py
@@ -255,7 +255,6 @@ def process_batch(
     consensus_kicks = []
     reported_regions = []
     internal_kicks = []
-    overlap_kicks = []
 
     total_x_before = 0
     total_x_after = 0
@@ -330,50 +329,6 @@ def process_batch(
         ref_average_data_length = sum(ref_average_data_length) / len(
             ref_average_data_length
         )
-
-        header_to_hits = defaultdict(list)
-        for node in nodes:
-            header_to_hits[node.base_header.split("_")[1]].append(node)
-        
-        for node, hits in header_to_hits.items():
-            overlap_kick_uids = set()
-            for (i, hit), (j, hit_b) in combinations(enumerate(hits), 2):
-                if i in overlap_kick_uids:
-                    continue
-                if j in overlap_kick_uids:
-                    continue
-
-                overlap_coords = get_overlap(hit.start, hit.end, hit_b.start, hit_b.end, 1)
-                amount_of_overlap = 0 if overlap_coords is None else overlap_coords[1] - overlap_coords[0]
-                distance = (hit_b.end - hit_b.start) + 1
-                percentage_of_overlap = amount_of_overlap / distance
-
-                if percentage_of_overlap >= 0.8:
-                        overlap_kick_uids.add(j)
-                        hits[j] = None
-                        if args.debug:
-                            overlap_kicks.append(
-                                f"{hit_b.header},{hit_b.score},{hit_b.start},{hit_b.end},Same Header Overlap Lowest Score,{hit.header},{hit.score},{start},{end}"
-                            )
-            header_to_hits[node] = hits
-            
-
-        nodes = []
-        for node, hits in header_to_hits.items():
-            for i, hit in enumerate(hits):
-                if hit is None:
-                    continue
-                final_node_score = hit.score
-                for j, hit_b in enumerate(hits):
-                    if hit_b is None:
-                        continue
-                    if i == j:
-                        continue
-    
-                    final_node_score += hit_b.score
-                            
-                hit.score = final_node_score
-                nodes.append(hit)
 
         nodes, internal_log, internal_header_kicks = internal_filter_gene(nodes, args.debug, gene, args.min_overlap_internal, args.score_diff_internal)
         kicked_headers.update(internal_header_kicks)
@@ -496,7 +451,6 @@ def process_batch(
         total_x_after += has_x_after
 
     return (
-        overlap_kicks,
         consensus_kicks,
         internal_kicks,
         reported_regions,
@@ -576,14 +530,12 @@ def do_folder(args: HmmfilterArgs, input_path: str):
         with Pool(args.processes) as pool:
             results = pool.map(process_batch, batched_arguments)
 
-    overlap_log = []
     consensus_log = []
     internal_log = []
     reported_regions = []
     total_x_before = 0
     total_x_after = 0
-    for overlap_kicks, consensus_kicks, internal_kicks, reported_regions, chunk_x_before, chunk_x_after in results:
-        overlap_log.extend(overlap_kicks)
+    for consensus_kicks, internal_kicks, reported_regions, chunk_x_before, chunk_x_after in results:
         consensus_log.extend(consensus_kicks)
         internal_log.extend(internal_kicks)
         reported_regions.extend(reported_regions)
@@ -605,12 +557,8 @@ def do_folder(args: HmmfilterArgs, input_path: str):
             fp.write("\n".join(reported_regions))
         
         with open(path.join(hmmfilter_path, "internal_kicks.txt"), "w") as fp:
-            fp.write("Kicked Gene,Header,Frame,Pool Score,Start,End,Reason,Master Gene,Header,Frame,Pool Score,Start,End\n")
-            fp.write("\n".join(internal_log))
-
-        with open(path.join(hmmfilter_path, "overlap_kicks.txt"), "w") as fp:
             fp.write("Kicked Gene,Header,Frame,Score,Start,End,Reason,Master Gene,Header,Frame,Score,Start,End\n")
-            fp.write("\n".join(overlap_log))
+            fp.write("\n".join(internal_log))
 
     printv(f"Done! Took {time_keeper.differential():.2f} seconds", args.verbose, 1)
 
