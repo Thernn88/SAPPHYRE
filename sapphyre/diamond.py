1b49b859e954e1c63a5cbf43f3786b1bdce594a9
Ashton Pooley <ashton.p404@gmail.com>
2024-03-12 14:23:40 +1030

    Skip matching top alignments

--- sapphyre/diamond.py
+++ sapphyre/diamond.py
@@ -522,9 +522,9 @@ def top_reference_realign(orthoset_raw_path, orthoset_aln_path, top_refs, target
         writeFasta(out_path, out)
         return
     
-    # if path.exists(out_path):
-    #     if len(out) == len(list(parseFasta(out_path, True))):
-    #         return
+    if path.exists(out_path):
+        if len(out) == len(list(parseFasta(out_path, True))):
+            return
     
     with NamedTemporaryFile(dir=gettempdir(), prefix=f"{gene}_") as tmp_prealign, NamedTemporaryFile(dir=gettempdir(), prefix=f"{gene}_") as tmp_result:
         tmp_prealign.write("\n".join([f">{i}\n{j}" for i, j in out]).encode())
