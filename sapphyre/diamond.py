8aecdebe5ec82fe2c0953c63be2d1ca9bc9e93aa
Ashton Pooley <ashton.p404@gmail.com>
2024-08-05 10:09:02 +0930

    Use a function to count taxon

--- sapphyre/diamond.py
+++ sapphyre/diamond.py
@@ -507,6 +507,31 @@ def get_head_to_seq(nt_db, recipe):
     return head_to_seq
 
 
+def count_taxa(df, target_to_taxon, genome_score_filter, is_assembly_or_genome):
+    # Initialize structures
+    combined_count = Counter()
+
+    # Apply genome score filter if necessary
+    if is_assembly_or_genome and genome_score_filter:
+        filtered_df = df[df["score"] > genome_score_filter]
+    else:
+        filtered_df = df
+
+    # Create a mapping of target to ref_taxa
+    filtered_df['ref_taxa'] = filtered_df['target'].map(lambda x: target_to_taxon[x][1])
+
+    # Keep only unique (header, ref_taxa) pairs
+    unique_pairs = filtered_df[['header', 'ref_taxa']].drop_duplicates()
+
+    # Count the number of unique headers for each ref_taxa
+    ref_taxa_counts = unique_pairs['ref_taxa'].value_counts()
+
+    # Update the combined count
+    combined_count.update(ref_taxa_counts.to_dict())
+    most_common = combined_count.most_common()
+
+    return most_common
+
 def delete_empty_columns(records):
     """Delete empty columns from an alignment.
 
@@ -892,38 +917,9 @@ def run_process(args: Namespace, input_path: str) -> bool:
     global_log = []
     dupe_divy_headers = defaultdict(set)
 
-    # Initialize structures
-    taxon_to_targets = defaultdict(list)
-    target_to_gene = {}
-    combined_count = Counter()
-
-    # Populate taxon_to_targets and target_to_gene
-    for target in df["target"].unique():
-        gene, ref_taxa, _ = target_to_taxon[target]
-        taxon_to_targets[ref_taxa].append(target)
-        target_to_gene[target] = gene
-
-    # Apply genome score filter if necessary
-    if is_assembly_or_genome and genome_score_filter:
-        filtered_df = df[df["score"] > genome_score_filter]
-    else:
-        filtered_df = df
-
-    # Create a mapping of target to ref_taxa
-    filtered_df['ref_taxa'] = filtered_df['target'].map(lambda x: target_to_taxon[x][1])
-
-    # Keep only unique (header, ref_taxa) pairs
-    unique_pairs = filtered_df[['header', 'ref_taxa']].drop_duplicates()
-
-    # Count the number of unique headers for each ref_taxa
-    ref_taxa_counts = unique_pairs['ref_taxa'].value_counts()
-
-    # Update the combined count
-    combined_count.update(ref_taxa_counts.to_dict())
-
-    top_refs = set()
+    most_common = count_taxa(df, target_to_taxon, genome_score_filter, is_assembly_or_genome)
     
-    most_common = combined_count.most_common()
+    top_refs = set()
     with open(path.join(input_path, "diamond_top_ref.csv"), "w") as fp:
         fp.write("Global count\n")
         for k, v in most_common:
